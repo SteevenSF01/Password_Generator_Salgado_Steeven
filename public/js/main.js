e77a6c0ecb8e5317dbee0ca734e427d52ee88d6b
Steeven Salgado <salgadosteeven@gmail.com>
2024-01-21 21:26:16 +0100

    ajout des commentaires

--- public/js/main.js
+++ public/js/main.js
@@ -23,7 +23,7 @@ let symbols = document.querySelector("#symbols")
 let buttonGenerate = document.querySelector(".btnGenerate")
 
 
-// arrays 
+// arrays  //! Creation des tableaux
 let upperCharacter = ["A","B","C","D","E","F","G","H","J","K","M","N","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let lowerCharacter = ["a","b","c","d","e","f","g","h","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z"];
 let numbersCharacter = ["0","1","2","3","4","5","6","7","8","9"];
@@ -31,11 +31,11 @@ let symbolsCharacter = ["!","#","$","%","&","*","+","-","?","@","§,","_",")","^
 
 
 
-let password = ""
-let arrayCharacter = []
+let password = ""  //! variable vide pour stocker le mot de passe
+let arrayCharacter = [] //! Le tableau qui va servir a concat les tableaux
 
 function getRandomPassword() {
-    if (uppercase.checked) {
+    if (uppercase.checked) { //! Si la condition est vraie => le tableau va concat
         arrayCharacter = arrayCharacter.concat(upperCharacter)
     }
     if (lowercase.checked) {
@@ -49,7 +49,7 @@ function getRandomPassword() {
         arrayCharacter = arrayCharacter.concat(symbolsCharacter)
 
     }
-    if (lenghtPassword.value > 1) {
+    if (lenghtPassword.value > 1) { //! la longueur ne peut pas être inférieur à 1
         for (let index = 0; index < lenghtPassword.value; index++) {
             let randomIndex = Math.floor(Math.random() * arrayCharacter.length)
             password = password + arrayCharacter[randomIndex]
@@ -62,10 +62,10 @@ function getRandomPassword() {
 
 buttonGenerate.addEventListener("click", function () {
     password = ""
-    if (lenghtPassword.value <= 0) {
-        document.querySelector(".lenght0").style.display = "block"
+    if (lenghtPassword.value <= 0) { //! si la longueur est == 0, il y aura un message d'erreur
+        document.querySelector(".lenght0").style.display = "block"  //! Block pour afficher
         setTimeout(() => {
-            document.querySelector(".lenght0").style.display = "none"
+            document.querySelector(".lenght0").style.display = "none" //! none pour le cacher et avec un timer de 1 seconde
         }, 1000);
     }
     inputPasseword.value = getRandomPassword()
@@ -73,14 +73,14 @@ buttonGenerate.addEventListener("click", function () {
 
 buttonCopy.addEventListener("click", () => {
     buttonCopy.value = inputPasseword.value
-    if (inputPasseword.value == "") {
+    if (inputPasseword.value == "") { //! Si l'input est vide, un message s'affiche 
         document.querySelector(".messageError").style.display = "block"
         setTimeout(() => {
             document.querySelector(".messageError").style.display = "none"
         }, 1500);
         return
     }
-    navigator.clipboard.writeText(inputPasseword.value).then(() => {
+    navigator.clipboard.writeText(inputPasseword.value).then(() => { //! Une fois copier, un message s'affiche 
         document.querySelector(".message").style.display = "block";
 
         setTimeout(() => {
